290fb2880721348594c99be0f97301fced10733f
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2021-07-21 08:58:09 -0400

    Transform + interpolate points into SVG path

--- react/src/Graph/Graph.js
+++ react/src/Graph/Graph.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import "./Graph.css";
 
-export function Graph() {
+export function Graph({frameX = 100, frameY = 100, data = []}) {
+  const {ys, xs} = data.reduce((set, [x, y]) => ({
+    xs: [...set.xs, x],
+    ys: [...set.ys, y],
+  }), {xs: [], ys: []});
+
+  const domain = {
+    min: Math.min(...xs),
+    max: Math.max(...xs),
+  };
+
+  const range = {
+    min: Math.min(...ys),
+    max: Math.max(...ys),
+  };
+
+  const scale = {
+    x: frameX / (domain.max - domain.min),
+    y: frameY / (range.max - range.min),
+  };
+
+  const path = data
+    .sort(([ax], [bx]) => ax - bx)
+    .map(([x, y], index) => {
+      const dx = (x - domain.min) * scale.x;
+      const dy = frameY - ((y - range.min) * scale.y);
+
+      return index < 1 ? `M${dx} ${dy}` : `L${dx} ${dy}`;
+    })
+    .join(' ');
+
   return (
-    <svg className="graph" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <title>graph-id</title>
-      <path d="M0 50 L20 80 L40 60 L70 70 L100 40" stroke="black" strokeWidth="1" fill="transparent" />
+    <svg className="graph" viewBox={`0 0 ${frameX} ${frameY}`} xmlns="http://www.w3.org/2000/svg">
+      <path d={path} stroke="#006aff" strokeWidth="1" fill="transparent" />
     </svg>
   );
 }
--- react/src/index.js
+++ react/src/index.js
@@ -4,32 +4,41 @@ import { Graph } from './Graph';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
+const data = [
+  [[0, 1], [1, 2], [2, 4], [3, 8], [4, 16]],
+  [[-10, 10], [4, -10], [0, 0], [7, 6], [9, 4]],
+  [[10000, 46000], [10004, 60000], [10007, 69000], [10014, 82000], [10021, 42500], [10026, 40000]],
+  [[1, 0.84], [2, 0.95], [4, 0.97], [5, 0.87], [6, 1.01], [9, 0.96], [10, 0.99], [12, 1.04], [16, 1.01]],
+  [[0.124, 0.3342], [0.132, 0.4056], [0.36, 0.4267], [0.4, 0.4], [0.422, 0.3691]],
+  [[10, 10], [20, 10], [40, 10]],
+];
+
 function Playground() {
   return (
     <div className="playground">
       <label>75px &#xD7; 75px</label>
-      <div className="ex-1">
-        <Graph />
+      <div className="ex-0">
+        <Graph frameX={100} frameY={100} data={data[0]} />
       </div>
       <label>200px &#xD7; 200px</label>
-      <div className="ex-2">
-        <Graph />
+      <div className="ex-1">
+        <Graph data={data[1]} />
       </div>
       <label>275px &#xD7; 100%</label>
-      <div className="ex-3">
-        <Graph />
+      <div className="ex-2">
+        <Graph frameX={250} frameY={100} data={data[2]} />
       </div>
       <label>225px &#xD7; 50%</label>
-      <div className="ex-4">
-        <Graph />
+      <div className="ex-3">
+        <Graph data={data[3]} />
       </div>
       <label>400px &#xD7; 20%</label>
-      <div className="ex-5">
-        <Graph />
+      <div className="ex-4">
+        <Graph data={data[4]} />
       </div>
       <label>100vh &#xD7; 100%</label>
-      <div className="ex-6">
-        <Graph />
+      <div className="ex-5">
+        <Graph data={data[5]} />
       </div>
     </div>
   );
