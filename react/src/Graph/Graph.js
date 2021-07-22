9f04003e4d3450df94b9d24dbd3bcd7bd3673a42
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2021-07-22 10:46:15 -0400

    Add cardinal 0.5 cubic bezier to graph path

--- react/src/Graph/Graph.js
+++ react/src/Graph/Graph.js
@@ -2,12 +2,12 @@ import React from 'react';
 import "./Graph.css";
 
 function interpolateXY(point, domain, range, frameX, frameY, spread) {
-  const dx = domain.min !== domain.max ?
-    (frameX * (point.x - domain.min)) / (domain.max - domain.min)
+  const dx = domain.min !== domain.max
+    ? (frameX * (point.x - domain.min)) / (domain.max - domain.min)
     : 0;
 
   const delta = Math.abs((range.max - range.min) / range.min);
-  const compression = delta < spread ? frameY * (1 - (delta / spread)) : 0;
+  const compression = delta < spread ? frameY * (1 - (delta / spread)) : frameY * 0.05;
   let dy = frameY - (compression / 2) - (((frameY - compression) * (point.y - range.min)) / (range.max - range.min));
 
   if (!isFinite(dy)) {
@@ -15,6 +15,35 @@ function interpolateXY(point, domain, range, frameX, frameY, spread) {
   }
 
   return { dx, dy };
+}
+
+function cubicBezier(points) {
+  const bezierPoints = [];
+
+  for (let i = 0; i < points.length - 1; i++) {
+    const set = [
+      { ...points[Math.max(i - 1, 0)] },
+      { ...points[i] },
+      { ...points[i + 1] },
+      { ...points[Math.min(i + 2, points.length - 1)] },
+    ];
+
+    const bezier = [
+      {
+        x: (-set[0].x + (6 * set[1].x) + set[2].x) / 6,
+        y: (-set[0].y + (6 * set[1].y) + set[2].y) / 6,
+      },
+      {
+        x: (set[1].x + (6 * set[2].x) - set[3].x) / 6,
+        y: (set[1].y + (6 * set[2].y) - set[3].y) / 6,
+      },
+      { ...set[2] }
+    ];
+
+    bezierPoints.push(bezier);
+  }
+
+  return bezierPoints;
 }
 
 export function Graph({ data = [], frameX = 100, frameY = 100, spread = 0 }) {
@@ -33,14 +62,17 @@ export function Graph({ data = [], frameX = 100, frameY = 100, spread = 0 }) {
     max: Math.max(...ys),
   };
 
-  const path = data
+  const points = data
     .sort(({x: ax}, {x: bx}) => ax - bx)
-    .map((point, index) => {
-      const { dx, dy } = interpolateXY(point, domain, range, frameX, frameY, spread);
+    .map((point) => {
+      const { dx: x, dy: y } = interpolateXY(point, domain, range, frameX, frameY, spread);
 
-      return index < 1 ? `M${dx} ${dy}` : `L${dx} ${dy}`;
-    })
-    .join(' ');
+      return { x, y };
+    });
+
+  const path = [`M${points[0].x} ${points[0].y}`].concat(
+    cubicBezier(points).map(([a, b, c]) => `C${a.x} ${a.y}, ${b.x} ${b.y}, ${c.x} ${c.y}`)
+  ).join(' ');
 
   return (
     <svg className="graph" viewBox={`0 0 ${frameX} ${frameY}`} xmlns="http://www.w3.org/2000/svg">
