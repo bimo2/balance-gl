49012d9dfd68f7888f88dedef19bd9260b917e8c
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2021-08-07 01:12:00 -0400

    Add basic svg test

--- react/tests/GLGraph.test.tsx
+++ react/tests/GLGraph.test.tsx
@@ -1,12 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { GLGraph } from 'GLGraph/GLGraph';
 
-test('renders svg paths', async () => {
-  render(<GLGraph view={{ x: 100, y: 100 }} data={[]} />);
+it('renders svg graph', () => {
+  const { container } = render(
+    <GLGraph
+      view={{ x: 200, y: 100 }}
+      data={[
+        { x: 0, y: 1 },
+        { x: 1, y: 2 },
+        { x: 2, y: 4 },
+        { x: 3, y: 8 },
+        { x: 4, y: 16 },
+      ]}
+    />
+  );
 
-  const path = await screen.findByText((_, element) => element?.tagName === 'path');
+  const [path] = container.querySelectorAll('path');
 
   expect(path).toBeInTheDocument();
   expect(path).toHaveAttribute('stroke', '#000');
+  expect(path).toHaveAttribute('stroke-width', '2.5');
   expect(path).toHaveAttribute('fill', 'transparent');
 });
